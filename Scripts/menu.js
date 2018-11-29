c31c131abffaf59a004c29a3d9567a6440137d69
davidTheSalad <davidsamuelcoelho@gmail.com>
2018-11-29 16:17:56 +0000

    Add files via upload

---
+++ Scripts/menu.js
@@ -0,0 +1,53 @@
+﻿window.onload = function() {
+    document.getElementById("ambientMenu").play();
+	var x = document.getElementById("optionsMenu");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+muteaudio = function() {
+    document.getElementById("ambientMenu").pause();
+}
+unmuteaudio = function() {
+    document.getElementById("ambientMenu").play();
+}
+function myFunction() {
+/* x = document.getElementById("allthethings");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }*/
+var x = document.getElementById("single");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+var x = document.getElementById("multiplayer");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+var x = document.getElementById("options");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+var x = document.getElementById("credits");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+	/*var x = document.getElementById("optionsMenu");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }*/
+  }
