11dd5ed9a58f930e9b7c2c4789fc2f98d253f145
X <davidsamuelcoelho@gmail.com>
2020-06-21 12:22:49 +0100

    Update menu.js

--- Scripts/menu.js
+++ Scripts/menu.js
@@ -60,7 +60,8 @@ document.getElementById("vid").play();
 			el2.style.display="block";
 			el.style.display="block";
 
-
+		comecou = 'false';
+			
 		if(this.currentTime >= 113.6)
 		{
 			if (!comecou)
